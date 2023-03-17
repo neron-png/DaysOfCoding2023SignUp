1d0a52cdaa67804b19e8f7acfea0aa4ceeafaded
Neron-png <neronmkp@csd.auth.gr>
2023-03-17 18:11:09 +0200

    Added main functionality to the server applet

--- server.py
+++ server.py
@@ -3,3 +3,25 @@ from flask_pymongo import PyMongo
 from flask_cors import CORS
 import sqlite3
 
+app = Flask(__name__)
+CORS(app)
+ip = "localhost"
+DATABASE = 'reviews.db'
+connection = sqlite3.connect(DATABASE, check_same_thread=False)
+cursor = connection.cursor()
+
+@app.route("/", methods=["GET"])
+def search():
+
+    reviews = cursor.execute("""
+    SELECT review
+    FROM reviews
+    """)
+    
+    review_comments = []
+    for review in reviews:
+        review_comments.append(review[0])
+
+    return Response(status=200)
+
+app.run(host=ip, port=5000, debug=False)
